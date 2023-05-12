b4ab2b5db8fa615ca9296ccba53c669e9d72dd53
RaphaelHeibel <raphaelrheibel@gmail.com>
2023-05-12 14:47:27 -0300

    validacao do DepartmentId antes de gravar no BD

---
+++ src/Migrations/20230512174118_DepartmentForeignKey.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMvcApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class DepartmentForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Seller",
+                newName: "Email");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Seller",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Seller",
+                newName: "email");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "BirthDate",
+                table: "Seller",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+    }
+}
--- src/Services/SellerService.cs
+++ src/Services/SellerService.cs
@@ -13,6 +13,7 @@ namespace src.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
