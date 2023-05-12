cb623d583ed9e2d0a9b6d0aa9e02608b7d3f998e
RaphaelHeibel <raphaelrheibel@gmail.com>
2023-05-12 14:37:52 -0300

    [feature] create seller form

--- src/Controllers/SellersController.cs
+++ src/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using src.Models;
 using src.Services;
 
 namespace src.Controllers
@@ -13,6 +14,19 @@ namespace src.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- src/Services/SellerService.cs
+++ src/Services/SellerService.cs
@@ -3,12 +3,24 @@ using src.Models;
 
 namespace src.Services
 {
-    public class SellerService 
+    public class SellerService
     {
         private readonly SalesContext _context;
-        
+
         public SellerService(SalesContext context) => _context = context;
 
         public List<Seller> FindAll() => _context.Seller.ToList();
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Seller obj)
+        {
+            _context.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
